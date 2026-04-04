d446d563828b5493301dc00dc409ccc97fc58750
alirahimifachr <alirahimifachr@gmail.com>
2026-04-04 14:01:57 +0200

    fix: android layout

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
+  .then(() => {
+    if (Capacitor.isNativePlatform()) {
+      StatusBar.setOverlaysWebView({ overlay: true });
+      StatusBar.setStyle({ style: Style.Light });
+    }
+  })
   .catch((err) => console.error(err));
